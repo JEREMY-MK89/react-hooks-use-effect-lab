e6e28566f5dff6ac9fda917597a40b23468fcd17
moses <moses.kingi@moringaschool.com>
2023-12-03 19:33:25 +0300

    sideEffect hook Lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // This function will be called when the component mounts
+    const timer = setTimeout(() => {
+      // Decrease the time remaining by 1 every second
+      setTimeRemaining((prevTime) => Math.max(0, prevTime - 1));
+    }, 1000);
+
+    // Clean up the timer when the component unmounts or when timeRemaining hits 0
+    return () => clearTimeout(timer);
+  }, [timeRemaining]); // Add timeRemaining as a dependency
+
+  useEffect(() => {
+    // Check if timeRemaining has reached 0
+    if (timeRemaining === 0) {
+      // Reset timeRemaining to 10 seconds
+      setTimeRemaining(10);
+      // Call the onAnswered callback with a value of false
+      onAnswered(false);
+    }
+  }, [timeRemaining, onAnswered]); // Add timeRemaining and onAnswered as dependencies
 
   function handleAnswer(isCorrect) {
+    // Reset timeRemaining to 10 seconds
     setTimeRemaining(10);
+    // Call the onAnswered callback with the provided value
     onAnswered(isCorrect);
   }
 
